143187a8dca3d457d367c9897d9878083260da03
omraval18 <r.auckwell18@gmail.com>
2025-08-29 02:26:14 +0530

    container prisma issue fixed, ci added

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import { projectRoutes } from "./routers/project.routes";
 import { env } from "./env";
 
 const app = new Hono<{
-	Variables: AuthType
+  Variables: AuthType;
 }>();
 
 if (env.NODE_ENV === "development") {
@@ -22,47 +22,49 @@ if (env.NODE_ENV === "development") {
   console.log("CORS_ORIGIN:", env.CORS_ORIGIN);
 }
 
-app.use("/*", cors({
-  origin: env.CORS_ORIGIN,
-  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.use("*", async (c, next) => {
-    if (c.req.path.startsWith("/api/inngest")) {
-        return next();
-    }
-
-    const session = await auth.api.getSession({ headers: c.req.raw.headers });
-
-    if (!session) {
-        if (env.NODE_ENV === "development") {
-          console.error("No session found, user is not authenticated");
-        }
-        c.set("user", null);
-        c.set("session", null);
-        return next();
-    }
-
-    c.set("user", session.user);
-    c.set("session", session.session);
+  if (c.req.path.startsWith("/api/inngest")) {
     return next();
+  }
+
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    if (env.NODE_ENV === "development") {
+      console.error("No session found, user is not authenticated");
+    }
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
 });
 
 app.on(
-    ["GET", "PUT", "POST"],
-    "/api/inngest",
-    serve({
-        client: inngest,
-        functions,
-    })
+  ["GET", "PUT", "POST"],
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  }),
 );
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
-app.route("/api/", uploadRoutes)
+app.route("/api/", uploadRoutes);
 app.route("/api/", clipRoutes);
-app.route("/api/",projectRoutes)
-
+app.route("/api/", projectRoutes);
 
 app.get("/", (c) => {
   return c.text("OK");
