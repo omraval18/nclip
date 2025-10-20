b362d1a1e28064a252e07c2293b79c1786a102dc
omraval18 <r.auckwell18@gmail.com>
2025-10-20 13:27:37 +0530

    fix: credit and plan not req for auth

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -4,35 +4,32 @@ import { db } from "./db";
 import { env } from "@/env";
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql",
-    }),
-    trustedOrigins: [env.CORS_ORIGIN || ""],
-    emailAndPassword: {
-        enabled: true,
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [env.CORS_ORIGIN || ""],
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      plan: {
+        type: "string",
+        required: false,
+        defaultValue: "free",
+        input: false,
+      },
+      credits: {
+        type: "number",
+        required: false,
+        defaultValue: 0,
+        input: false,
+      },
     },
-    user: {
-        additionalFields: {
-            plan: {
-                type: "string",
-                required: true,
-                defaultValue: "free",
-                input:false
-            },
-            credits: {
-                type: "number",
-                required: true,
-                defaultValue: 0,
-                input:false
-            },
-        },
-    },
+  },
 });
 
-
 export type AuthType = {
-    user: typeof auth.$Infer.Session.user | null;
-    session: typeof auth.$Infer.Session.session | null;
+  user: typeof auth.$Infer.Session.user | null;
+  session: typeof auth.$Infer.Session.session | null;
 };
-
-
